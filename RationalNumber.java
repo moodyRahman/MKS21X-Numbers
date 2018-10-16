a3fdfeb82584912fb6b542ff1e345a4cea1a3113
Moody <mrahman01@stuy.edu>
2018-10-15 21:50:12 -0400

    added RationalNumber asccessors

--- RationalNumber.java
+++ RationalNumber.java
@@ -1,10 +1,21 @@
 public class RationalNumber extends RealNumber{
+  private int num;
+  private int den;
   RationalNumber(double input){
     super(input);
   }
 
   RationalNumber(int num, int den){
     super((num+0.0)/(den+0.0));
+    this.num = num;
+    this.den = den;
+  }
+
+  public int getNum(){
+    return num;
+  }
+  public int getDen(){
+    return den;
   }
 
 }
