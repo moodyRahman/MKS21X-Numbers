4d64ee8c9bb29287ddb6af26ce60165b23407b76
Moody <mrahman01@stuy.edu>
2018-10-15 21:54:21 -0400

    added comparing operators in RealNumber

--- RealNumber.java
+++ RealNumber.java
@@ -9,7 +9,15 @@ public class RealNumber{
   public double getValue(){
     return number;
   }
+
   public double truncate(){
     return 4.56;
+  }
+
+  public boolean greaterThan(RealNumber inp){
+    return this.number > inp.getValue();
+  }
+  public static boolean greaterThan(RealNumber inp1, RealNumber inp2){
+    return inp1.getValue() > inp2.getValue();
   }
 }
